c89eccbf7e7f30038b0eeebfd46de390df377138
BWallyn <ben96.wallyn@gmail.com>
2024-08-21 15:37:06 +0200

    add dates info

--- src/velib_prediction/pipelines/feature_engineering/pipeline.py
+++ src/velib_prediction/pipelines/feature_engineering/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.19.7
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import get_holidays, split_train_test
+from .nodes import extract_date_features, get_holidays, split_train_test
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -22,7 +22,19 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs=["df_with_bool_cols_upd", "params:feat_date", "params:delta_days"],
                 outputs=["df_train", "df_test"],
                 name="split_train_test"
-            )
+            ),
+            node(
+                func=extract_date_features,
+                inputs=["df_train", "params:feat_date"],
+                outputs="df_train_w_date_feat",
+                name="Add_date_feat_train"
+            ),
+            node(
+                func=extract_date_features,
+                inputs=["df_test", "params:feat_date"],
+                outputs="df_test_w_date_feat",
+                name="Add_date_feat_test"
+            ),
         ],
         inputs=["df_with_bool_cols_upd"],
         namespace="feature_engineering"
