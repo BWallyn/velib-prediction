199158a213fc72caa74494dd18913f91c0dc13ad
BWallyn <ben96.wallyn@gmail.com>
2024-08-22 10:13:17 +0200

    add holidays info

--- src/velib_prediction/pipelines/feature_engineering/pipeline.py
+++ src/velib_prediction/pipelines/feature_engineering/pipeline.py
@@ -5,7 +5,12 @@ generated using Kedro 0.19.7
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import extract_date_features, get_holidays, split_train_test
+from .nodes import (
+    add_holidays_period,
+    extract_date_features,
+    get_holidays,
+    split_train_test,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -34,6 +39,42 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs=["df_test", "params:feat_date"],
                 outputs="df_test_w_date_feat",
                 name="Add_date_feat_test"
+            ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_train_w_date_feat", "df_holidays", "params:feat_date", "params:zone_a"],
+                outputs="df_train_w_holidays",
+                name="add_holidays_zone_a_train"
+            ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_train_w_holidays", "df_holidays", "params:feat_date", "params:zone_b"],
+                outputs="df_train_w_holidays_b",
+                name="add_holidays_zone_b_train"
+            ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_train_w_holidays_b", "df_holidays", "params:feat_date", "params:zone_c"],
+                outputs="df_train_w_holidays_c",
+                name="add_holidays_zone_c_train"
+            ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_test_w_date_feat", "df_holidays", "params:feat_date", "params:zone_a"],
+                outputs="df_test_w_holidays",
+                name="add_holidays_zone_a_test"
+            ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_test_w_holidays", "df_holidays", "params:feat_date", "params:zone_b"],
+                outputs="df_test_w_holidays_b",
+                name="add_holidays_zone_b_test"
+            ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_test_w_holidays_b", "df_holidays", "params:feat_date", "params:zone_c"],
+                outputs="df_test_w_holidays_c",
+                name="add_holidays_zone_c_test"
             ),
         ],
         inputs=["df_with_bool_cols_upd"],
