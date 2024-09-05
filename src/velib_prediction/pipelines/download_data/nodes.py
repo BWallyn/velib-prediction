a727a6b8a1d81a709153f8d2dc36adab81fcec4a
BWallyn <ben96.wallyn@gmail.com>
2024-09-05 08:39:44 +0200

    create pipeline download data

---
+++ src/velib_prediction/pipelines/download_data/nodes.py
@@ -0,0 +1,15 @@
+"""
+This is a boilerplate pipeline 'download_data'
+generated using Kedro 0.19.7
+"""
+# =================
+# ==== IMPORTS ====
+# =================
+
+import requests
+
+# ===================
+# ==== FUNCTIONS ====
+# ===================
+
+
