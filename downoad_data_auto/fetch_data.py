3e67f223ad2f3d8ce7ff6f64d66d041841e1aebd
BWallyn <ben96.wallyn@gmail.com>
2024-09-06 08:59:48 +0200

    function to download and save data

---
+++ downoad_data_auto/fetch_data.py
@@ -0,0 +1,9 @@
+from ..src.velib_prediction.pipelines.download_data.nodes import (
+    download_data,
+    save_data,
+)
+
+
+def main():
+    df_velib = download_data()
+    save_data(df_velib, path_data="data/01_raw")
