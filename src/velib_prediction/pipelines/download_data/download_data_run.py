a8970513ecc3802f5036d461568a133c1c828f12
BWallyn <ben96.wallyn@gmail.com>
2024-10-17 07:35:11 +0200

    fix paths

--- src/velib_prediction/pipelines/download_data/download_data_run.py
+++ src/velib_prediction/pipelines/download_data/download_data_run.py
@@ -27,7 +27,7 @@ def main(url: str, path_data: str) -> None:
 if __name__ == '__main__':
     # Options
     URL = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?refine=nom_arrondissement_communes%3A%22Paris%22&refine=nom_arrondissement_communes%3A%22Levallois-Perret%22&refine=nom_arrondissement_communes%3A%22Puteaux%22&refine=nom_arrondissement_communes%3A%22Suresnes%22&refine=nom_arrondissement_communes%3A%22Boulogne-Billancourt%22&refine=nom_arrondissement_communes%3A%22Clichy%22"
-    PATH_DATA = 'data/01_raw/'
+    PATH_DATA = '../../../../data/01_raw/'
 
     # Run
     main(url=URL, path_data=PATH_DATA)
