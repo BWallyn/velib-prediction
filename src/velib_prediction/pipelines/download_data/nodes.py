9aa24adf09b0a63844186672a7835ce7b2914007
BWallyn <ben96.wallyn@gmail.com>
2024-10-17 07:33:53 +0200

    move run of script

--- src/velib_prediction/pipelines/download_data/nodes.py
+++ src/velib_prediction/pipelines/download_data/nodes.py
@@ -55,29 +55,3 @@ def save_data(df: pd.DataFrame, path_data: str) -> None:
     logger.info('Saving dataset...')
     df.to_parquet(os.path.join(path_data, f"velib_{timestamp}.parquet"))
     logger.info('Dataset saved')
-
-
-def main(url: str, path_data: str) -> None:
-    """Download and save data
-
-    Args:
-        url (str): Path to the dataset online
-        path_data (str): Path to the folder where to store the dataset downloaded
-    Returns:
-        None
-    """
-    df = download_data(url)
-    save_data(df, path_data=path_data)
-
-
-# =============
-# ==== RUN ====
-# =============
-
-if __name__ == '__main__':
-    # Options
-    URL = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?refine=nom_arrondissement_communes%3A%22Paris%22&refine=nom_arrondissement_communes%3A%22Levallois-Perret%22&refine=nom_arrondissement_communes%3A%22Puteaux%22&refine=nom_arrondissement_communes%3A%22Suresnes%22&refine=nom_arrondissement_communes%3A%22Boulogne-Billancourt%22&refine=nom_arrondissement_communes%3A%22Clichy%22"
-    PATH_DATA = 'data/01_raw/'
-
-    # Run
-    main(url=URL, path_data=PATH_DATA)
