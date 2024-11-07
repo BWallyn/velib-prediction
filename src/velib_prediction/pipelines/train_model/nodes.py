e9ad89af7a6e143caccd3a159873e6898a2e9b9b
BWallyn <ben96.wallyn@gmail.com>
2024-11-07 08:52:49 +0100

    change parameter name

--- src/velib_prediction/pipelines/train_model/nodes.py
+++ src/velib_prediction/pipelines/train_model/nodes.py
@@ -23,9 +23,9 @@ from velib_prediction.pipelines.train_model.mlflow import (
 # ===================
 
 def add_lags_sma(  # noqa: PLR0913
-    df: pd.DataFrame, list_lags: list[int], feat_id: str, feat_date: str, feat_target: str, shift_days: int,
+    df: pd.DataFrame, list_lags: list[int], feat_id: str, feat_date: str, feat_target: str, n_shift: int,
 ) -> pd.DataFrame:
-    """Add different lags to the dataset with a shift of shift_days.
+    """Add different lags to the dataset with a shift of n_shift
 
     Args:
         df (pd.DataFrame): Input dataframe
@@ -33,14 +33,14 @@ def add_lags_sma(  # noqa: PLR0913
         feat_id (str): Name of the id column of the velib stations
         feat_date (str): Name of the date column
         feat_target (str): Name of the target column
-        shift_days (int): Number of days to shift the results
+        n_shift (int): Number of rows to shift the results
     Returns:
         df (pd.DataFrame): Output dataframe with lags added
     """
     df = df.sort_values(by=[feat_date])
     for id in df[feat_id].unique():
         for lag in list_lags:
-            df.loc[df[feat_id] == id, f'sma_{lag}_lag'] = df.loc[df[feat_id] == id].rolling(lag)[feat_target].mean().shift(shift_days).values
+            df.loc[df[feat_id] == id, f'sma_{lag}_lag'] = df.loc[df[feat_id] == id].rolling(lag)[feat_target].mean().shift(n_shift).values
     return df
 
 
