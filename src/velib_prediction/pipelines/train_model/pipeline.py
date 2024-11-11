e4e660fc3c148323cb38123619c197b4b33780ff
BWallyn <ben96.wallyn@gmail.com>
2024-11-11 14:48:01 +0100

    pipeline to train model using cross validation

--- src/velib_prediction/pipelines/train_model/pipeline.py
+++ src/velib_prediction/pipelines/train_model/pipeline.py
@@ -8,8 +8,9 @@ from kedro.pipeline import Pipeline, node, pipeline
 from velib_prediction.pipelines.train_model.nodes import (
     add_lags_sma,
     get_split_train_val_cv,
+    train_model_cv_mlflow,
 )
-from velib_prediction.utils.utils import rename_columns
+from velib_prediction.utils.utils import drop_columns, rename_columns, sort_dataframe
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -35,10 +36,35 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="Add_lag_to_dataset",
             ),
             node(
+                func=sort_dataframe,
+                inputs=["df_train_w_lags", "params:feat_date"],
+                outputs="df_sorted",
+                name="Sort_dataframe_by_date",
+            ),
+            node(
+                func=drop_columns,
+                inputs=["df_sorted", "params:feat_date"],
+                outputs="df_w_date_col",
+                name="Drop_date_column",
+            ),
+            node(
                 func=get_split_train_val_cv,
-                inputs=["df_train_w_lags", "params:n_splits"],
+                inputs=["df_w_date_col", "params:n_splits"],
                 outputs="list_train_valid",
                 name="Create_split_expanding_windows"
+            ),
+            node(
+                func=train_model_cv_mlflow,
+                inputs=[
+                    "params:run_name",
+                    "params:experiment_id",
+                    "list_train_valid",
+                    "params:list_feat_cat",
+                    "params:verbose",
+                    "params:catboost_parameters",
+                ],
+                outputs="model",
+                name="Train_catboost_model_using_cross_validation"
             ),
         ],
         inputs="df_train_prepared",
