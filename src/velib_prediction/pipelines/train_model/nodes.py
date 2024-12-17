9ac06c731581209c5d995d9bb342c23162ef7f4d
BWallyn <ben96.wallyn@gmail.com>
2024-12-17 07:29:33 +0100

    Function to train final model

--- src/velib_prediction/pipelines/train_model/nodes.py
+++ src/velib_prediction/pipelines/train_model/nodes.py
@@ -134,7 +134,7 @@ def create_mlflow_experiment_if_needed(
 
 
 
-def train_model(
+def _train_model(
     pool_train: Pool, pool_eval: Pool, plot_training: bool, verbose: int,
     **kwargs,
 ) -> CatBoostRegressor:
@@ -205,7 +205,7 @@ def train_model_mlflow(  # noqa: PLR0913
         nested=True,
         tags={mlflow.utils.mlflow_tags.MLFLOW_PARENT_RUN_ID: parent_run_id}
     ) as child_run:
-        model = train_model(pool_train, pool_eval, plot_training=False, verbose=verbose, **kwargs)
+        model = _train_model(pool_train, pool_eval, plot_training=False, verbose=verbose, **kwargs)
         # Predict
         pred_train = model.predict(pool_train)
         pred_eval = model.predict(pool_eval)
@@ -297,7 +297,7 @@ def optimize_hyperparams(  # noqa: PLR0913
 
     # Train model mlflow
     logger.info(f"Train Catboost model using {optimize_params}")
-    _, _, rmse_valid = train_model_mlflow(
+    model, _, rmse_valid = train_model_mlflow(
         experiment_id=experiment_id,
         parent_run_id=run_id,
         df_train=df_train,
@@ -307,6 +307,8 @@ def optimize_hyperparams(  # noqa: PLR0913
         **optimize_params
     )
     logger.info(f"Catboost model trained with RMSE: {rmse_valid}")
+    # Add the best iteration as an attribute
+    trial.set_user_attr("best_iter", model.get_best_iteration())
     return rmse_valid
 
 
@@ -351,57 +353,62 @@ def train_model_bayesian_opti(  # noqa: PLR0913
         # Optimize
         study.optimize(objective, n_trials=n_trials, show_progress_bar=True)
         logger.info(f"Best parameters found: {study.best_params}")
-    return study.best_params
+    # Get best parameters
+    best_parameters = study.best_params
+    # Set the number of iterations as the best iteration
+    best_parameters["iterations"] = study.best_trial.user_attrs["best_iter"]
+    return best_parameters
 
 
-def optimize_hyperparams_cv(
+def train_final_model(  # noqa: PLR0913
     experiment_id: str,
-    run_id: str,
-    search_params: dict[str, Any],
-    list_train_valid: list[tuple[pd.DataFrame, pd.DataFrame]],
+    df_train: pd.DataFrame,
+    df_valid: pd.DataFrame,
     feat_cat: list[str],
+    best_parameters: dict[str, Any],
+    verbose: int=0,
 ):
-    """
-    """
-    pass
+    """Train a Catboost regressor model and log the parameters, metrics, model and shap values to MLflow
 
-
-def train_model_bayesian_opti_cv(  # noqa: PLR0913
-    run_name: str,
-    experiment_id: str,
-    search_params: dict[str, Any],
-    list_train_valid: list[tuple[pd.DataFrame, pd.DataFrame]],
-    feat_cat: list[str],
-    n_trials: int
-) -> dict[str, Any]:
-    """Use Bayesian optimization to find the best hyperparameters using cross validation
+    - Define a Catboost regressor model
+    - Train the model on training set and use a validation set to keep the best model
+    - Predict on train and evaluation sets
+    Log parameters, model, metrics, shap and confusion matrix to MLflow
 
     Args:
-        run_name (str): Name of the run
         experiment_id (str): Id of the MLflow experiment
-        search_params (dict[str, Any]): Hyperparameters space to search
-        list_train_valid (list[tuple[pd.DataFrame, pd.DataFrame]]): Tuples of train and validation sets
-        feat_cat (list[str]): List of the categorical features
-        n_trials (int): Number of trials for the bayesian optimization
+        parent_run_id (str): Id of the MLflow parent run
+        df_train (pd.DataFrame): Train DataFrame
+        df_valid (pd.DataFrame): Validation DataFrame
+        verbose (int): verbose parameter while learning
+        **kwargs: hyperparameters of the Catboost regressor
     Returns:
-        (dict[str, Any]): Best hyperparameters of the model
+        model: Catboost regressor model trained
     """
-    # Start a MLflow run
-    with mlflow.start_run(run_name=run_name, experiment_id=experiment_id) as parent_run:
-        # Run Bayesian optimization using Optuna
-        study = optuna.create_study(study_name="", direction="maximize")
-
-        # Define objective function
-        objective = partial(
-            optimize_hyperparams_cv,
-            experiment_id=experiment_id,
-            run_id=parent_run.info.run_id,
-            search_params=search_params,
-            list_train_valid=list_train_valid,
-            feat_cat=feat_cat,
-        )
-
-        # Optimize
-        study.optimize(objective, n_trials=n_trials, show_progress_bar=True)
-        logger.info(f"Best parameters found: {study.best_params}")
-    return study.best_params
+    # Select the target
+    y_train = df_train["target"]
+    y_valid = df_valid["target"]
+    x_train = df_train.drop(columns=["target"])
+    x_valid = df_valid.drop(columns=["target"])
+    # Create pools for Catboost model
+    pool_train = Pool(data=x_train, label=y_train, cat_features=feat_cat)
+    pool_eval = Pool(data=x_valid, label=y_valid, cat_features=feat_cat)
+    with mlflow.start_run(experiment_id=experiment_id) as parent_run:
+        # Set model
+        model = _train_model(pool_train, None, plot_training=False, verbose=verbose, **best_parameters)
+        # Predict
+        pred_train = model.predict(pool_train)
+        pred_eval = model.predict(pool_eval)
+        # Log parameters to MLflow
+        _log_mlflow_catboost_parameters(model=model)
+        # Log metrics
+        rmse_train = root_mean_squared_error(y_true=y_train, y_pred=pred_train)
+        rmse_valid = root_mean_squared_error(y_true=y_valid, y_pred=pred_eval)
+        dict_metrics = {
+            'rmse_train': rmse_train,
+            'rmse_valid': rmse_valid,
+        }
+        _log_mlflow_metric(dict_metrics=dict_metrics, run_id=parent_run.info.run_id)
+        # Log model
+        _log_mlflow_model_catboost(model=model, df=df_train)
+    return model, rmse_train, rmse_valid
