dff9a0b5b2b834f05da5f4ea1cb88a5aed328545
BWallyn <ben96.wallyn@gmail.com>
2025-02-06 08:15:11 +0100

    add function to merge holidays period

--- src/velib_prediction/pipelines/feature_engineering/pipeline.py
+++ src/velib_prediction/pipelines/feature_engineering/pipeline.py
@@ -6,8 +6,7 @@ generated using Kedro 0.19.7
 from kedro.pipeline import Pipeline, node, pipeline
 
 from velib_prediction.pipelines.feature_engineering.nodes import (
-    # add_holidays_period,
-    drop_columns,
+    add_holidays_period,
     extract_date_features,
     get_holidays,
     split_train_test,
@@ -43,24 +42,24 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="Add_date_feat_test"
             ),
             # TODO fix holidays merge
-            # node(
-            #     func=add_holidays_period,
-            #     inputs=["df_train_w_date_feat", "df_holidays", "params:feat_date_holidays", "params:zone_a"],
-            #     outputs="df_train_w_holidays",
-            #     name="add_holidays_zone_a_train"
-            # ),
-            # node(
-            #     func=add_holidays_period,
-            #     inputs=["df_train_w_holidays", "df_holidays", "params:feat_date_holidays", "params:zone_b"],
-            #     outputs="df_train_w_holidays_b",
-            #     name="add_holidays_zone_b_train"
-            # ),
-            # node(
-            #     func=add_holidays_period,
-            #     inputs=["df_train_w_holidays_b", "df_holidays", "params:feat_date_holidays", "params:zone_c"],
-            #     outputs="df_train_w_holidays_c",
-            #     name="add_holidays_zone_c_train"
-            # ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_training_date", "df_holidays", "params:feat_date_holidays", "params:zone_a"],
+                outputs="df_train_w_holidays_a",
+                name="add_holidays_zone_a_train"
+            ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_train_w_holidays_a", "df_holidays", "params:feat_date_holidays", "params:zone_b"],
+                outputs="df_train_w_holidays_b",
+                name="add_holidays_zone_b_train"
+            ),
+            node(
+                func=add_holidays_period,
+                inputs=["df_train_w_holidays_b", "df_holidays", "params:feat_date_holidays", "params:zone_c"],
+                outputs="df_train_w_holidays_c",
+                name="add_holidays_zone_c_train"
+            ),
             # node(
             #     func=add_holidays_period,
             #     inputs=["df_test_w_date_feat", "df_holidays", "params:feat_date_holidays", "params:zone_a"],
@@ -78,12 +77,6 @@ def create_pipeline(**kwargs) -> Pipeline:
             #     inputs=["df_test_w_holidays_b", "df_holidays", "params:feat_date_holidays", "params:zone_c"],
             #     outputs="df_test_w_holidays_c",
             #     name="add_holidays_zone_c_test"
-            # ),
-            # node(
-            #     func=drop_columns,
-            #     inputs=["df_train_w_date_feat", "params:cols_to_drop"],
-            #     outputs="df_train_prepared",
-            #     name="Drop_unused_columns",
             # ),
             node(
                 func=validate_dataset,
